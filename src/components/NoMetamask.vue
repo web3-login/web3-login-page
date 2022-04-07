24867af25cff7703e52bd0210305bbb15c815f90
Christian M <chriamue@gmail.com>
2022-04-07 10:31:30 +0200

    :sparkles: added coinbase link

--- src/components/NoMetamask.vue
+++ src/components/NoMetamask.vue
@@ -3,13 +3,22 @@
     <span>You will need</span
     ><a href="https://metamask.io/"
       ><img
-        alt="metamask"
+        alt="metamask wallet"
         src="../assets/metamask-fox.svg"
         width="68"
         height="60"
       /> </a
     ><a href="https://metamask.io/"><span>METAMASK</span></a
-    ><span>&nbsp; to sign a message</span>
+    ><span>&nbsp;or&nbsp;</span>
+    <a href="https://metamask.io/"
+      ><img
+        alt="coinbase wallet"
+        src="../assets/Coinbase_Wordmark.svg"
+        width="160"
+        height="60"
+      /> </a
+    ><a href="https://www.coinbase.com/wallet"></a>
+    <span>&nbsp; Wallet to sign a message</span>
   </div>
 </template>
 <script>
@@ -31,5 +40,15 @@ export default {
   font-size: 24px;
   color: #ffffff;
   line-height: 60px;
+}
+a {
+  text-decoration: none;
+}
+a:link,
+a:visited {
+  color: white;
+}
+a:hover {
+  color: white;
 }
 </style>
